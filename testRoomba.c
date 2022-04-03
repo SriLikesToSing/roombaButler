180be4d6a0be82406be8d4e436b4e3c026808a8a
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-04-03 15:35:17 -0700

    Update testRoomba.c

--- testRoomba.c
+++ testRoomba.c
@@ -8,8 +8,6 @@ int txPin = 11; //connected to pin 3 (RXD) of iRobot Create 2
 int ddPin = 5;  //connected to pin 5 (BRC) of iRobot Create 2
 int COMMAND_ONE = 1;
 int COMMAND_TWO = 2;
-int motorPin = 4;
-int motorPin2 = 6;
 
 //IR stuff
 int LED = 13; // Use the onboard LED
@@ -20,6 +18,11 @@ int left = HIGH;
 int counter = 0;
 bool initiation = true;
 
+//motorLeft defines
+int speedPinLeft = 9;
+int direction1Left = 8;
+int direction2Left = 7;
+int mSpeedLeft = 200;
 
 
 //Set up a new Software Serial Port
@@ -82,6 +85,12 @@ void driveStop(void){
 
 
 void setup() {
+  
+  //MotorLeft setup
+  pinMode(speedPinLeft, OUTPUT);
+  pinMode(direction1Left, OUTPUT);
+  pinMode(direction2Left, OUTPUT);
+  
 
   Serial.begin(9600);
 
@@ -118,18 +127,23 @@ void setup() {
   Serial.println("Start OI in Safe Mode.");
   softSerial.write(128); //Starts the OI
   softSerial.write(131); //Set mode to Safe
+
+
+  
   
   
    
-  
   
 
 }
 
 void loop() {
-    digitalWrite(motorPin, HIGH);
-    digitalWrite(motorPin2, HIGH);
-/*
+
+  digitalWrite(direction1Left, HIGH);
+  digitalWrite(direction2Left, LOW);
+  analogWrite(speedPinLeft, mSpeedLeft);
+  Serial.println("BRUH");
+ /*   
     while(BT.available()){
       delay(10);
       //...
@@ -138,42 +152,43 @@ void loop() {
         SWITCH = 0;
       }else if(state == "massage"){
         SWITCH = 1;
-      }else{
+      }else{ 
         SWITCH = -1;
       }
       
     }
+  
     
     digitalWrite(motorPin, HIGH);
-
-
-   while(1){
-    
-     * if(SWITCH == -1){
-     * break;
-     * }
   
+
+  while(1){
+
+   
   right = digitalRead(isObstaclePin);
   left = digitalRead(isObstaclePin2);
 
   if (right == LOW && left == HIGH){
-    
-    
     driveLeft();
   }else if(right == HIGH && left == LOW){
     
     driveRight();
   }else if(right == HIGH && left == HIGH){
     driveStop();
-    delay(1000);
-    
-    delay(3000);
+    //delay(1000);
+    //digitalWrite(motorPin, HIGH);
+   // digitalWrite(motorPin2, HIGH);
+   
     
     //once drive stop == 2 then restart the entire system
   }else if(right == LOW && left == LOW){
     driveStraight();
+   }
   }
-*/
+  */
+  
+
+   
 }
 
  
